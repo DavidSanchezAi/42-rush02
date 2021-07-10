29975ddd93b80a8621ffff031d2db28046f7c785
DavidSanchezAi <86553169+DavidSanchezAi@users.noreply.github.com>
2021-07-10 13:30:06 +0200

    Add files via upload

--- pruebas/ft_search.c
+++ pruebas/ft_search.c
@@ -1,14 +1,40 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <fcntl.h>
+//
+#include <stdio.h>
+
+long int findSize(char file_name[])
+{
+    // opening the file in read mode
+	FILE* fp = fopen(file_name, "r");
+  
+    // checking if the file exist or not
+	if (fp == NULL) {
+		printf("File Not Found!\n");
+		return -1;
+    }
+  
+	fseek(fp, 0L, SEEK_END);
+
+	// calculating the size of the file
+	long int res = ftell(fp);
+
+	// closing the file
+	fclose(fp);
+  
+	return res;
+}
+
+
 int main(void)
 {
-	char buf[30];
 	size_t nbytes;
 	ssize_t bytes_read;
 	int fd = open("foo.txt", O_RDONLY);;
 	int i;
-
+	char buf[findSize("foo.txt")];
+	
 	nbytes = sizeof(buf);
 	bytes_read = read(fd, buf, nbytes);
 
